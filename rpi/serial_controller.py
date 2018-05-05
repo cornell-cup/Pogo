86bd5b15aa36dfa12e865ed8a8ed3e0e5c1d1609
Justin Kuang <zk65@cornell.edu>
2018-05-05 00:13:24 +0000

    started the process serial class and callbacks for each device

--- rpi/serial_controller.py
+++ rpi/serial_controller.py
@@ -1,9 +1,60 @@
 import serial
 import time
 
-s = serial.Serial('/dev/ttyACM1')
-print(s.isOpen())
-print(s.inWaiting())
+s0 = serial.Serial('/dev/ttyACM0')
+s1 = serial.Serial('/dev/ttyACM1')
+
+#Reassign as necessary, plug them in the order you want.
+s_motor = s0
+s_imu = s1
+
+processMotorSerial = new ProcessSerial("motor", s_motor, processMotorPacket)
+
+def processMotorPacket(packet):
+    if !packet:
+        print "Error: Packet Empty"
+    elif len(packet) == 0:
+        print "Error: Packet Empty"
+    elif packet[0] == 'Q':
+        print "Motor shutdown packet"
+    else:
+        print ''.join(packet)
+
+def processImuPacket(packet):
+    if !packet:
+        print "Error: Packet Empty"
+    elif len(packet) == 0:
+        print "Error: Packet Empty"
+    elif packet[0] == 'Q':
+        print "Motor shutdown packet"
+    else:
+        print ''.join(packet)
+
+
+class ProcessSerial():
+
+
+    def __init__(self, name, port, processPacket):
+        self.name = name
+        self.port = port
+        self.processPacket = processPacket
+        self.buffer = []
+        self.hasStart = false
+        self.hasEnd = false
+        
+        
+    def readSerial():
+	while s.inWaiting() > 0:
+	    inByte = s.read(1)
+	    if inByte == '\2':
+                self.hasStart = true
+                self.buffer = []
+                
+                
+        
+
+
+
 
 while 1:
 	input = raw_input(">> ")
