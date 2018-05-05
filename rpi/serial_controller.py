368b4c5045a902043afa292b2fa544172dc646d2
Justin Kuang <zk65@cornell.edu>
2018-05-05 01:45:52 +0000

    Got python running, reads serial propperly

--- rpi/serial_controller.py
+++ rpi/serial_controller.py
@@ -2,29 +2,58 @@ import serial
 import time
 
 s0 = serial.Serial('/dev/ttyACM0')
-s1 = serial.Serial('/dev/ttyACM1')
+#s1 = serial.Serial('/dev/ttyACM1')
 
 #Reassign as necessary, plug them in the order you want.
 #Use dmesg command in the terminal to determine which one is which.
 s_motor = s0
-s_imu = s1
+s_imu = s0
 
 imu_rotation = (0.0,0.0,0.0)
 imu_position = (0.0,0.0,0.0)
 motor_rpm = 0
 
-processMotorSerial = new ProcessSerial("motor", s_motor, processMotorPacket)
-processImuSerial = new ProcessSerial("imu", s_imu, processImuPacket)
+class ProcessSerial:
 
-SerialReaders = []
-SerialReaders += processMotorSerial
-SerialReaders += processImuSerial
-#SerialReaders += processRadioSerial
-#SerialReaders += processSolenoidSerial
+    def __init__(self, name, port, processPacket):
+        self.name = name
+        self.port = port
+        self.processPacket = processPacket
+        self.buffer = []
+        self.hasStart = False
+        
+        
+    def readSerial(self):
+	while self.port.inWaiting() > 0:
+	    inByte = self.port.read(1)
+	    if inByte == '\2':
+                self.hasStart = True
+                self.buffer = []
+            elif inByte == '\3':
+                self.processPacket(self.buffer)
+                self.reset()
+            else:
+                if not self.hasStart:
+                    1==1
+                #the packet is too long, longer than any of our set packets
+                elif len(self.buffer) > 10:
+                    self.reset()
+                else:
+                    self.buffer += inByte
+
+
+    def reset(self):
+        self.buffer = []
+        self.hasStart = False
+
+    def closePort(self):
+        self.port.close()
+                
+                
 
 
 def processMotorPacket(packet):
-    if !packet:
+    if not packet:
         print "Error: Packet Empty"
     elif len(packet) == 0:
         print "Error: Packet Empty"
@@ -37,7 +66,7 @@ def processMotorPacket(packet):
         print ''.join(packet)
 
 def processImuPacket(packet):
-    if !packet:
+    if not packet:
         print "Error: Packet Empty"
     elif len(packet) == 0:
         print "Error: Packet Empty"
@@ -52,52 +81,22 @@ def processImuPacket(packet):
         print ''.join(packet)
 
 
-class ProcessSerial:
 
-
-    def __init__(self, name, port, processPacket):
-        self.name = name
-        self.port = port
-        self.processPacket = processPacket
-        self.buffer = []
-        self.hasStart = false
         
-        
-    def readSerial():
-	while self.port.inWaiting() > 0:
-	    inByte = self.port.read(1)
-	    if inByte == '\2':
-                self.hasStart = true
-                self.buffer = []
-            elif inByte == '\3':
-                self.processPacket(self.buffer)
-                self.reset()
-            else:
-                if !self.hasStart:
-                    1==1
-                #the packet is too long, longer than any of our set packets
-                elif len(self.buffer) > 10:
-                    self.reset()
-                else:
-                    self.buffer += inByte
+processMotorSerial = ProcessSerial("motor", s_motor, processMotorPacket)
+processImuSerial = ProcessSerial("imu", s_imu, processImuPacket)
 
-
-    def reset():
-        self.buffer = []
-        self.hasStart = false
-
-    def closePort():
-        self.port.close()
-                
-                
-        
-
+SerialReaders = []
+#SerialReaders += processMotorSerial
+SerialReaders.append(processImuSerial)
+#SerialReaders += processRadioSerial
+#SerialReaders += processSolenoidSerial
 
 
 
 while 1:
     #read from devices to update info
-    for sr in SerialReader:
+    for sr in SerialReaders:
         sr.readSerial()
     #use info to calculate what to return
     #write serial to devices
@@ -106,5 +105,5 @@ while 1:
 
     
 
-for sr in SerialReader:
+for sr in SerialReaders:
     sr.closePort()
