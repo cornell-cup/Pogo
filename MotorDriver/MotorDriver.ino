fa8a87ecaa2acdff4f37055edbc3e8d066c65b9a
arklian <hjc77@cornell.edu>
2018-05-08 13:39:54 -0400

    Added vescUart library

--- MotorDriver/MotorDriver.ino
+++ MotorDriver/MotorDriver.ino
@@ -1,6 +1,28 @@
+#include "VescUart.h"
+#include "datatypes.h"
 
 #define OutputSerial false
 #define MaxPacketSize 40
+
+
+struct bldcMeasure measuredValues;
+
+unsigned long time;
+unsigned long prev_time;
+unsigned long tx_time;
+unsigned long count;
+
+bool read_rpm = false;
+bool isRunning = false;
+
+float error = 0.0;
+
+#define MAX_CURRENT 20.0f
+#define CURRENT_ACCEL 0.05f
+#define CURRENT_DECEL 0.20f
+float current = 0.0;
+bool accel = true;
+
 
 char inByte = 0;   // for incoming serial data
 
@@ -83,21 +105,24 @@ void resetSerial(){
   hasStart = false;
   bufferPos = 0;
   packetSize = -1;
+  serialBuffer[0] = 0;
   
 }
 
 void processPacket(){
   //Use serialBuffer
   //TODO for each;
-  int test_int = serialReadInt(0);
-  double test_float = serialReadDouble(4);
-  Serial.println(test_int);
-  Serial.println(test_float);
+  if (serialBuffer[0] == '\1' && packetSize == 5){
+    error = serialReadFloat(1);    
+  }
 }
 
 void setup() {
-  Serial.begin( 9600 );
+  //Serial is to the rpi
+  Serial.begin( 115200 );
   Serial.flush();
+  //Serial1 is to the motor controller
+  Serial1.begin( 9600 );
   delay( 1000 );
 }
 
@@ -131,6 +156,68 @@ void loop() {
   }
 
 
+  /*
+   * Possible Values
+  Loop: 137
+  avgMotorCurrent: 0.00
+  avgInputCurrent: 0.00
+  dutyCycleNow: -0.00
+  rpm: 0
+  inputVoltage: 23.90
+  ampHours: 0.00
+  ampHoursCharges: 0.00
+  tachometer: -2
+  tachometerAbs: 2  
+  */
+
+  prev_time = time;
+  time = millis();
+  //Serial.println( time - prev_time );
+
+  
+  if( !read_rpm ) {
+    //We send a request to the motor controller to send us back data.
+    //VescUartGetValue(measuredValues);
+    tx_time = time;  
+    read_rpm = true;
+  }
+  else {
+    //We need to wait for the packet to come back before we read it.
+    if( false &&  time - tx_time  > 160 ) {
+      if( VescUartReadValue(measuredValues) ) {
+        Serial.print("Loop: "); Serial.println(count++);
+        SerialPrint(measuredValues);
+        read_rpm = false;
+      }
+      else {
+        Serial.print('E');
+        read_rpm = false;
+      }
+    }
+  }
+
+  if (accel) {
+    if (current < MAX_CURRENT - 0.5f) {
+      current += CURRENT_ACCEL;
+    }
+    else {
+      accel = false;
+    }
+    VescUartSetCurrent(current);
+  }
+  else {
+    if (current > -MAX_CURRENT + 0.5f) {
+      current -= CURRENT_ACCEL;
+    }
+    else {
+      accel = true;
+    }
+    VescUartSetCurrent(current);
+
+  }
+  Serial.println(current);
+
+
 
   //Write output back to master
   if(OutputSerial){
@@ -141,6 +228,6 @@ void loop() {
     Serial.print('\x01');
   }
   
-  delay(10);
+//  delay(10);/
 }
 
