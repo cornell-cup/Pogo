7c6ac5a98ce0ea3923518b3c0e763905ffd3e2a0
Justin Kuang <zk65@cornell.edu>
2018-05-13 17:52:35 -0400

    solenoid control processing- shutoff, jump

--- SolenoidDriver/SolenoidDriver.ino
+++ SolenoidDriver/SolenoidDriver.ino
@@ -1,8 +1,113 @@
 #define NOT_DEBUG       // define as "DEBUG" to go into debugging mode with Serial Monitor
+#define OutputSerial false
+#define MaxPacketSize 40
+
+char inByte = 0;   // for incoming serial data
 
 // Pin assignments
 const int POT = A2;     // Linear potentiometer
 const int CONTROL = A3; // Logic for gate driver
+
+union DConv{
+   double x;
+   uint8_t c[sizeof (double)]; 
+};
+union DConv r_dconv;
+union DConv w_dconv;
+
+
+union FConv{
+   float x;
+   uint8_t c[sizeof (float)]; 
+};
+union FConv r_fconv;
+union FConv w_fconv;
+
+
+union IConv{
+   int x;
+   uint8_t c[sizeof (int)]; 
+};
+union IConv r_iconv;
+union IConv w_iconv;
+
+
+unsigned char serialBuffer[MaxPacketSize] = { 0 };
+bool hasStart = false;
+int bufferPos = 0;
+int packetSize = -1;
+
+int receivedPackets = 0;
+
+
+
+void serialPrintDouble(double val){
+  w_dconv.x = val;
+  for (unsigned i = 0; i < sizeof (double); i++){
+    Serial.write(w_dconv.c[i]);
+  }
+}
+
+void serialPrintFloat(float val){
+  w_fconv.x = val;
+  for (unsigned i = 0; i < sizeof (float); i++){
+    Serial.write(w_fconv.c[i]);
+  }
+}
+
+void serialPrintInt(int val){
+  w_iconv.x = val;
+  for (unsigned i = 0; i < sizeof (int); i++){
+    Serial.write(w_iconv.c[i]);
+  }
+}
+
+double serialReadDouble(int bufferPos){
+  for (unsigned i = 0; i < sizeof (double); i++) {
+    r_dconv.c[i] = serialBuffer[bufferPos + i];
+  }
+  return r_dconv.x;
+}
+
+float serialReadFloat(int bufferPos){
+  for (unsigned i = 0; i < sizeof (float); i++) {
+    r_fconv.c[i] = serialBuffer[bufferPos + i];
+  }
+  return r_fconv.x;
+}
+
+int serialReadInt(int bufferPos){
+  for (unsigned i = 0; i < sizeof (int); i++) {
+    r_iconv.c[i] = serialBuffer[bufferPos + i];
+  }
+  return r_iconv.x;
+}
+
+void resetSerial(){
+  hasStart = false;
+  bufferPos = 0;
+  packetSize = -1;
+  
+}
+
+
+//Packets:
+//0 -> Emergency Shutoff
+//1 -> Jump
+void processPacket(){
+  if (serialBuffer[0] == '\0' && packetSize == 1)
+  {
+    // shutoff();
+  }
+  else if (serialBuffer[0] == '\1' && packetSize == 1)
+  {
+  // jumps at max
+  solenoidControl( 294, 100 );
+  }
+
+  //always
+  potRead();
+}
 
 void setup() {
   #ifdef DEBUG
@@ -15,10 +120,65 @@ void setup() {
   pinMode( LED_BUILTIN, OUTPUT );
 }
 
+//from templateDriver
+// void setup() {
+//   Serial.begin( 9600 );
+//   Serial.flush();
+//   delay( 1000 );
+// }
+
 void loop() {
-  solenoidControl( 294, 100 );
-  potRead();
+
+
+  //Read Serial for any updates from Master
+  while ( Serial.available() > 0) {
+    inByte = Serial.read();
+    Serial.print(inByte);
+    if (!hasStart){
+      if ( inByte == '\2' ){
+
+        resetSerial();
+        hasStart = true;
+      } else {
+        //Serial.print("Has Noise");
+        
+      }
+    } else if (bufferPos > MaxPacketSize-2)  {
+      resetSerial();
+    } 
+    //second byte
+    else if ( packetSize == -1 ) {
+      packetSize = int(inByte);
+    } 
+    //rest of bytes
+    else { 
+      serialBuffer[bufferPos++] = inByte;
+    }
+
+    if ( packetSize != -1 && bufferPos == packetSize ){
+      processPacket();
+      receivedPackets++;
+      resetSerial();
+    }
+  }
+
+
+
+  //Write output back to master
+  if(OutputSerial){
+    //First byte is always '\2'
+    Serial.print('\2');
+    //Second byte is the size of the rest of the packet. Double * 8 + Float * 4 + Int * 4 + Char * 1 
+    Serial.print('\x09');
+    Serial.print('\x01');
+  }
+  
+  delay(10);
 }
+
+
+
+
 
 /**
  * Controls solenoid's jumping time and how much voltage control from Arduino
--- rpi/serial_controller.py
+++ rpi/serial_controller.py
@@ -5,8 +5,8 @@ import struct
 s0 = serial.Serial('/dev/ttyACM0', 115200)
 #s1 = serial.Serial('/dev/ttyACM1')
 
-#Reassign as necessary, plug them in the order you want.
-#Use dmesg command in the terminal to determine which one is which.
+# Reassign as necessary, plug them in the order you want.
+# Use dmesg command in the terminal to determine which one is which.
 s_motor = s0
 s_imu = s0
 
@@ -14,120 +14,120 @@ global imu_euler
 global imu_gyro
 global imu_linaccel
 global motor_rpm
-imu_euler = (0.0,0.0,0.0)
-imu_gyro = (0.0,0.0,0.0)
-imu_linaccel = (0.0,0.0,0.0)
+imu_euler = (0.0, 0.0, 0.0)
+imu_gyro = (0.0, 0.0, 0.0)
+imu_linaccel = (0.0, 0.0, 0.0)
 motor_rpm = 0
+
 
 class ProcessSerial:
 
-  #processPacket is a function
-  def __init__(self, name, port, processPacket):
-    self.name = name
-    self.port = port
-    self.processPacket = processPacket
-    self.buffer = bytearray()
-    self.hasStart = False
-    self.packetSize = -1
-    
-    
-  def readSerial(self):
-    inByte = '0'
-    while self.port.inWaiting() > 0:
-      inByte = self.port.read(1)
-      #print(inByte)
-      
-      if not self.hasStart:
-        if inByte == b'\x02':
-          self.reset()
-          self.hasStart = True
-        else:
-          print ("Has noise")
-      #the packet is too long, longer than any of our set packets
-      elif len(self.buffer) > 100:
-        self.reset()
-      elif self.packetSize == -1:
-        self.packetSize = int.from_bytes(inByte, byteorder='little', signed=False)
-      else:
-        self.buffer += inByte
-        
-      if self.packetSize != -1 and len(self.buffer) == self.packetSize :
-        self.processPacket(self.buffer)
-        self.reset()
-      
+    # processPacket is a function
+    def __init__(self, name, port, processPacket):
+        self.name = name
+        self.port = port
+        self.processPacket = processPacket
+        self.buffer = bytearray()
+        self.hasStart = False
+        self.packetSize = -1
 
+    def readSerial(self):
+        inByte = '0'
+        while self.port.inWaiting() > 0:
+            inByte = self.port.read(1)
+            # print(inByte)
 
-  def reset(self):
-    self.buffer = bytearray()
-    self.hasStart = False
-    self.packetSize = -1
+            if not self.hasStart:
+                if inByte == b'\x02':
+                    self.reset()
+                    self.hasStart = True
+                else:
+                    print("Has noise")
+            # the packet is too long, longer than any of our set packets
+            elif len(self.buffer) > 100:
+                self.reset()
+            elif self.packetSize == -1:
+                self.packetSize = int.from_bytes(
+                    inByte, byteorder='little', signed=False)
+            else:
+                self.buffer += inByte
 
-  def closePort(self):
-    self.port.close()
-        
-        
+            if self.packetSize != -1 and len(self.buffer) == self.packetSize:
+                self.processPacket(self.buffer)
+                self.reset()
+
+    def reset(self):
+        self.buffer = bytearray()
+        self.hasStart = False
+        self.packetSize = -1
+
+    def closePort(self):
+        self.port.close()
 
 
 def processMotorPacket(packet):
-  if not packet:
-    print ("Error: Packet Empty")
-  elif len(packet) == 0:
-    print ("Error: Packet Empty")
-  elif packet[0] == '0':
-    print ("Motor shutdown packet")
+    if not packet:
+        print("Error: Packet Empty")
+    elif len(packet) == 0:
+        print("Error: Packet Empty")
+    elif packet[0] == '0':
+        print("Motor shutdown packet")
 
-  #heartbeat
-  elif packet[0] == '1' and len(packet)==2:
-    if packet[1] == '0':
-      print('Emergency Shutoff')
-    if packet[1] == '1':
-      print('Motor On')
-    if packet[1] == '2':
-      print('Motor Off')
+    # heartbeat
+    elif packet[0] == '1' and len(packet) == 2:
+        if packet[1] == '0':
+            print('Emergency Shutoff')
+        if packet[1] == '1':
+            print('Motor On')
+        if packet[1] == '2':
+            print('Motor Off')
 
-  elif packet[0] == '2' and len(packet)==2:
-    if packet[1] == '0':
-      print('Motor turned off')
-    if packet[1] == '1':
-      print('Motor turned on')
+    elif packet[0] == '2' and len(packet) == 2:
+        if packet[1] == '0':
+            print('Motor turned off')
+        if packet[1] == '1':
+            print('Motor turned on')
 
-  elif packet[0] == '3' and len(packet) == 5:
-    global motor_rpm
-    #figure out if line 97 works, might have to conver to byte list
-    motor_rpm = int.from_bytes(packet[1:5], byteorder='little', signed=False)
-  else:
-    print (packet)
+    elif packet[0] == '3' and len(packet) == 5:
+        global motor_rpm
+        # figure out if line 97 works, might have to conver to byte list
+        motor_rpm = int.from_bytes(
+            packet[1:5], byteorder='little', signed=False)
+    else:
+        print(packet)
+
 
 def processImuPacket(packet):
-  if not packet:
-    print ("Error: Packet Empty")
-  elif len(packet) == 0:
-    print ("Error: Packet Empty")
-  elif packet[0] == '\x45':
-    print ("Imu Error")
-  elif packet[0] == 1 and len(packet) == 73:
-    global imu_euler
-    global imu_gyro
-    global imu_linaccel
-    euler_x = struct.unpack('d', bytes(packet[1:9]))
-    euler_y = struct.unpack('d', bytes(packet[9:17]))
-    euler_z = struct.unpack('d', bytes(packet[17:25]))
-    imu_euler = (euler_x,euler_y,euler_z)
-    gyro_x = struct.unpack('d', bytes(packet[25:33]))
-    gyro_y = struct.unpack('d', bytes(packet[33:41]))
-    gyro_z = struct.unpack('d', bytes(packet[41:49]))
-    imu_gyro = (gyro_x,gyro_y,gyro_z)
-    linacc_x = struct.unpack('d', bytes(packet[49:57]))
-    linacc_y = struct.unpack('d', bytes(packet[57:65]))
-    linacc_z = struct.unpack('d', bytes(packet[65:73]))
-    imu_linaccel = (linacc_x,linacc_y,linacc_z)
+    if not packet:
+        print("Error: Packet Empty")
+    elif len(packet) == 0:
+        print("Error: Packet Empty")
+    elif packet[0] == '\x45':
+        print("Imu Error")
+    elif packet[0] == 1 and len(packet) == 73:
+        global imu_euler
+        global imu_gyro
+        global imu_linaccel
+        euler_x = struct.unpack('d', bytes(packet[1:9]))
+        euler_y = struct.unpack('d', bytes(packet[9:17]))
+        euler_z = struct.unpack('d', bytes(packet[17:25]))
+        imu_euler = (euler_x, euler_y, euler_z)
+        gyro_x = struct.unpack('d', bytes(packet[25:33]))
+        gyro_y = struct.unpack('d', bytes(packet[33:41]))
+        gyro_z = struct.unpack('d', bytes(packet[41:49]))
+        imu_gyro = (gyro_x, gyro_y, gyro_z)
+        linacc_x = struct.unpack('d', bytes(packet[49:57]))
+        linacc_y = struct.unpack('d', bytes(packet[57:65]))
+        linacc_z = struct.unpack('d', bytes(packet[65:73]))
+        imu_linaccel = (linacc_x, linacc_y, linacc_z)
 
-  else:
-    print (packet)
+    else:
+        print(packet)
 
 
+def sendSolenoidPacket():
 
-    
+
 processMotorSerial = ProcessSerial("motor", s_motor, processMotorPacket)
 processImuSerial = ProcessSerial("imu", s_imu, processImuPacket)
 
@@ -137,24 +137,18 @@ SerialReaders.append(processImuSerial)
 #SerialReaders += processRadioSerial
 #SerialReaders += processSolenoidSerial
 
-print ( "Starting" )
+print("Starting")
 
 while 1:
-  #read from devices to update info
-  for sr in SerialReaders:
-    sr.readSerial()
-  #use info to calculate what to return
+    # read from devices to update info
+    for sr in SerialReaders:
+        sr.readSerial()
+    # use info to calculate what to return
 
-  global imu_euler
-  global imu_gyro
-  global imu_linaccel  
-  print (imu_euler)
-  #print (imu_position)
-  #print imu_rotation
-  #write serial to devices
-  
-
-
-  
-
-
+    global imu_euler
+    global imu_gyro
+    global imu_linaccel
+    print(imu_euler)
+    #print (imu_position)
+    # print imu_rotation
+    # write serial to devices
